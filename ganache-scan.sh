50535b39488132e686f4aa7c54faebc3d9e7e551
root <toufiqelahy@hotmail.com>
2022-06-02 18:27:28 +0200

    init

---
+++ ganache-scan.sh
@@ -0,0 +1,6 @@
+
+. $HOME/.asdf/asdf.sh
+export DATABASE_URL=postgresql://postgres:Ramestta123@localhost:5432/blockscout
+export ETHEREUM_JSONRPC_VARIANT=ganache
+export ETHEREUM_JSONRPC_HTTP_URL=http://103.150.136.231:8545
+export COIN=RAMA
